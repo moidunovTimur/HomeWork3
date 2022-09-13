86427c497c34daa8ab2f792a7e5c01803d78046b
moidunovTimur <moidunovtimur@gmail.com>
2022-09-13 21:46:25 +0600

    fff

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,22 @@ public class Main {
     public static void main(String[] args) {
         double[] nums = {10.9,2.6,3.6,7,-5.2,1.5,31.6,8.8,6.1,};
         boolean proverka = false;
-        for (double i:nums) {
-            if (i>0){
-                proverka=true;
-                System.out.println(i);
+        double count = 0;
+        int result = 0;
 
+        for (double i:nums) {
+            if (i<0){
+                proverka=true;
+
+            }else if (i>0 &&proverka){
+
+                if (i>0){
+                    count = count+i;
+
+                result++;
             }
 
-        }
+        }}
+        System.out.println(count / result);
     }
 }
